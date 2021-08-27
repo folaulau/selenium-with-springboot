5ad53533a33a8467d5249191ee34992f1cd7b164
Folau Kaveinga <folaukaveinga@gmail.com>
2021-08-26 18:50:53 -0600

    setup

---
+++ src/main/java/com/lovemesomecoding/selenium/SeleniumWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.selenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumWithSpringbootApplication.class, args);
+	}
+
+}
